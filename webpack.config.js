1492166f3ba77bdc001915d86ca9d6335fa68644
Maksim Hvatov <uzhas76064@gmail.com>
2024-06-13 09:41:05 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CopyPlugin=require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: "bundle.js",
+    clean: true
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "public" },
+      ],
+    }),
+  ],
+   devtool: 'source-map',
+   module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
