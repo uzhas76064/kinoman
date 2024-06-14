98c924f2fb0441695e6a8789cbaf2dfa8cbeade7
Maksim Hvatov <uzhas76064@gmail.com>
2024-06-14 11:01:46 +0300

    added ProfileView

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import FilmsView from './view/films-view.js';
 import {render, RenderPosition} from './render.js';
 import FilmCardView from './view/film-card-view.js';
-import SortView from "./view/sort-view.js";
+import SortView from './view/sort-view.js';
+import ProfileView from './view/profile-view.js';
 
 const siteMainElement = document.querySelector('.main');
+const headerElement = document.querySelector('.header');
 
+render(new ProfileView(), headerElement);
 render(new FilmsView(), siteMainElement);
 render(new SortView(), siteMainElement, RenderPosition.BEFOREBEGIN);
 
