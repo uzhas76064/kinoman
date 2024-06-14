3d20e2b33c8359685461259d4902f1371ce7c959
Maksim Hvatov <uzhas76064@gmail.com>
2024-06-14 13:30:41 +0300

    completed main-presenter.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,15 @@
-import FilmsView from './view/films-view.js';
-import {render, RenderPosition} from './render.js';
-import FilmCardView from './view/film-card-view.js';
-import SortView from './view/sort-view.js';
+import {render} from './render.js';
 import ProfileView from './view/profile-view.js';
-import FilterView from './view/filter-view';
-import ShowMoreView from './view/show-more-view';
-import FilmPopupView from './view/film-popup-view';
+// import FilmPopupView from './view/film-popup-view';
+import MainPresenter from './presenter/main-presenter';
 
-const bodyElement = document.querySelector('body');
+// const bodyElement = document.querySelector('body');
 const siteMainElement = document.querySelector('.main');
 const headerElement = document.querySelector('.header');
 
 render(new ProfileView(), headerElement);
-render(new FilmsView(), siteMainElement);
-render(new FilterView(), siteMainElement, RenderPosition.BEFOREBEGIN);
-render(new SortView(), siteMainElement, RenderPosition.AFTERBEGIN);
-render(new FilmPopupView(), bodyElement);
 
-const sectionFilms = document.querySelector('.films');
+// render(new FilmPopupView(), bodyElement);
 
-render(new FilmCardView(), sectionFilms);
-render(new ShowMoreView(), sectionFilms);
+const presenter= new MainPresenter();
+presenter.init(siteMainElement);
