188b996d73258671b8f5089704d0268c03c7b4df
Maksim Hvatov <uzhas76064@gmail.com>
2024-06-14 11:19:37 +0300

    added FilmPopupView

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import SortView from './view/sort-view.js';
 import ProfileView from './view/profile-view.js';
 import FilterView from './view/filter-view';
 import ShowMoreView from './view/show-more-view';
+import FilmPopupView from './view/film-popup-view';
 
+const bodyElement = document.querySelector('body');
 const siteMainElement = document.querySelector('.main');
 const headerElement = document.querySelector('.header');
 
@@ -13,6 +15,7 @@ render(new ProfileView(), headerElement);
 render(new FilmsView(), siteMainElement);
 render(new FilterView(), siteMainElement, RenderPosition.BEFOREBEGIN);
 render(new SortView(), siteMainElement, RenderPosition.AFTERBEGIN);
+render(new FilmPopupView(), bodyElement);
 
 const sectionFilms = document.querySelector('.films');
 
