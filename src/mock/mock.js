541b4b6b3058f6365525f0caf1460d6b14662496
Maksim Hvatov <uzhas76064@gmail.com>
2024-06-25 11:03:02 +0300

    changed structure

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,35 +1,27 @@
 export const generateMovie = () => ({
   id: 0,
-  comments: [
-    1,2
-  ],
+  comments: [],
   filmInfo: {
-    title: 'A Little Pony Without The Carpet',
-    alternativeTitle: 'Laziness Who Sold Themselves',
-    totalRating: 5.3,
-    poster: 'images/posters/blue-blazes.jpg',
+    title: '',
+    alternativeTitle: '',
+    totalRating: 0,
+    poster: '',
     ageRating: 0,
-    director: 'Tom Ford',
-    writers: [
-      'Takeshi Kitano'
-    ],
-    actors: [
-      'Morgan Freeman'
-    ],
+    director: '',
+    writers: [],
+    actors: [],
     release: {
-      date: '2019-05-11T00:00:00.000Z',
-      releaseCountry: 'Finland'
+      date: null,
+      releaseCountry: ''
     },
-    runtime: 77,
-    genre: [
-      'Comedy'
-    ],
-    description: 'Oscar-winning film, a war drama about two young people, from the creators of timeless classic "Nu, Pogodi!" and "Alice in Wonderland", with the best fight scenes since Bruce Lee.'
+    runtime: 0,
+    genre: [],
+    description: ''
   },
   userDetails: {
     watchlist: false,
     alreadyWatched: true,
-    watchingDate: '2019-04-12T16:12:32.554Z',
+    watchingDate: null,
     favorite: false
   }
 });
