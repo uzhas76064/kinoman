3537bf141c58a70e857aba62ed72bab9bbbd1e93
Maksim Hvatov <uzhas76064@gmail.com>
2024-06-28 11:21:47 +0300

    добавлена динамическая отрисовка информации о фильмах

--- src/view/film-card-view.js
+++ src/view/film-card-view.js
@@ -1,28 +1,45 @@
 import {createElement} from '../render';
 
-const createFilmCardTemplate = () => `<article class="film-card">
+const createFilmCardTemplate = (movie) => {
+  const {
+    title,
+    genre,
+    totalRating,
+    poster,
+    release,
+    runtime,
+    description} = movie.filmInfo;
+  const {date} = release;
+  const comments = movie.comments;
+
+  return ( `<article class="film-card">
           <a class="film-card__link">
-            <h3 class="film-card__title">Sagebrush Trail</h3>
-            <p class="film-card__rating">3.2</p>
+            <h3 class="film-card__title">${title}</h3>
+            <p class="film-card__rating">${totalRating}</p>
             <p class="film-card__info">
-              <span class="film-card__year">1933</span>
-              <span class="film-card__duration">54m</span>
-              <span class="film-card__genre">Western</span>
+              <span class="film-card__year">${date}</span>
+              <span class="film-card__duration">${runtime}m</span>
+              <span class="film-card__genre">${genre}</span>
             </p>
-            <img src="./images/posters/sagebrush-trail.jpg" alt="" class="film-card__poster">
-            <p class="film-card__description">Sentenced for a murder he did not commit, John Brant escapes from prison determined to find the real killer. By chance Brant's narrow escap…</p>
-            <span class="film-card__comments">89 comments</span>
+            <img src=${poster} alt=${title} class="film-card__poster">
+            <p class="film-card__description">${description}...</p>
+            <span class="film-card__comments">${comments.length > 1 ? `${comments.length} comments` : `${comments.length} comment`}</span>
           </a>
           <div class="film-card__controls">
             <button class="film-card__controls-item film-card__controls-item--add-to-watchlist film-card__controls-item--active" type="button">Add to watchlist</button>
             <button class="film-card__controls-item film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
             <button class="film-card__controls-item film-card__controls-item--favorite" type="button">Mark as favorite</button>
           </div>
-        </article>`;
+        </article>`);
+};
 
 export default class FilmCardView {
+  constructor(movie) {
+    this.movie = movie;
+  }
+
   getTemplate () {
-    return createFilmCardTemplate();
+    return createFilmCardTemplate(this.movie);
   }
 
   getElement () {
@@ -31,6 +48,7 @@ export default class FilmCardView {
     }
     return this.element;
   }
+
   removeElement () {
     this.element = null;
   }
