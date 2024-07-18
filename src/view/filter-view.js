a4dccd7fad4d3bb3ec1afbfa0672b586f4f6c473
Maksim Hvatov <uzhas76064@gmail.com>
2024-07-18 14:11:33 +0300

    генерация фильтров

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,11 +1,17 @@
 import AbstractView from '../framework/view/abstract-view';
+import {generateFilters} from '../utils';
 
-const createFilterViewTemplate = () => `<nav class="main-navigation">
-    <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-    <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-    <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-    <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
-  </nav>`;
+const createFilterViewTemplate = () => {
+  const filters = generateFilters();
+
+  return (`<nav class="main-navigation">
+<!--    <a href="#all" class="main-navigation__item main-navigation__item&#45;&#45;active">All movies</a>-->
+<!--    <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>-->
+<!--    <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>-->
+<!--    <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>-->
+        ${filters.map((filter) => `<a href="#${filter.toLowerCase()}" class="main-navigation__item">${filter}</a>`).join('')}
+  </nav>`);
+};
 
 export default class FilterView extends AbstractView{
   constructor() {
