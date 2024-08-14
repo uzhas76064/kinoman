2a5448841e9f592c31840eb8834e085637f51cfb
Maksim Hvatov <uzhas76064@gmail.com>
2024-08-14 13:17:02 +0500

    интеграция api-сервиса в точку входа

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import FilmsModel from './model/films-model';
 import CommentsModel from './model/comments-model';
 import FilterPresenter from "./presenter/filter-presenter";
 import FilterModel from "./model/filter-model";
+import FilmsApiService from "./services/films-api-service";
+
+const BASIC_ID = '1ge6sw36fv7';
+
+const END_POINT = 'https://17.ecmascript.htmlacademy.pro/cinemaddict';
+const AUTHORIZATION = `Basic ${BASIC_ID}`
 
 const siteMainElement = document.querySelector('.main');
 const headerElement = document.querySelector('.header');
@@ -18,6 +24,8 @@ const commentsModel = new CommentsModel(filmsModel);
 const filterPresenter = new FilterPresenter(siteMainElement, filmsModel, filterModel);
 const filmsPresenter= new FilmsPresenter(siteMainElement, filmsModel, commentsModel, filterModel);
 
+const filmsApiService = new FilmsApiService(END_POINT, AUTHORIZATION);
+
 filterPresenter.init();
 filmsPresenter.init();
-
+console.log(filmsApiService.films)
---
+++ src/services/films-api-service.js
@@ -0,0 +1,25 @@
+import ApiService from "../framework/api-service";
+
+const METHODS = {
+  GET: 'GET',
+  PUT: 'PUT',
+  DELETE: 'DELETE'
+}
+
+export default class FilmsApiService extends ApiService{
+  get films() {
+    return this._load({url: '/movies'})
+      .then(ApiService.parseResponse);
+  }
+
+  updateFilms = async (film) => {
+    const response = await this._load({
+      url: `/movies/${film.id}`,
+      method: METHODS.PUT,
+      body: JSON.stringify(film),
+      headers: new Headers({'Content-Type': 'application/json'})
+    })
+
+    return await ApiService.parseResponse(response);
+  }
+}
