186f5b6ed28fdec4066aa22e8f472dbfa04aeb3a
Maksim Hvatov <uzhas76064@gmail.com>
2024-08-15 13:04:03 +0500

    Реализован паттерн адаптер

--- src/model/films-model.js
+++ src/model/films-model.js
@@ -1,5 +1,6 @@
 import {generateFilms} from '../mock/films';
 import Observable from "../framework/observable";
+import FilmsApiService from "../services/films-api-service";
 
 export default class FilmsModel extends Observable{
   #filmApiService = null;
@@ -10,14 +11,49 @@ export default class FilmsModel extends Observable{
     this.#filmApiService = filmApiService;
 
     this.#filmApiService.films.then((films) => {
-      console.log(films)
+      console.log(films[0])
+      // console.log(this.#adaptToClient(films[0]))
+      console.log(films.map(this.#adaptToClient))
     })
   }
 
+  #deleteUnusedProps = (obj) => {
+    delete obj['film_info'];
+    delete obj.filmInfo['alternative_title'];
+    delete obj.filmInfo['total_rating'];
+    delete obj.filmInfo['age_rating'];
+    delete obj['user_details'];
+    delete obj.userDetails['watchlist'];
+    delete obj.userDetails['already_watched'];
+    delete obj.userDetails['watching_date'];
+  }
+
   #adaptToClient = (film) => {
+    const filmInfo = film['film_info'];
+
     const adaptedFilm = {
       ...film,
+      filmInfo: {
+        ...filmInfo,
+        alternativeTitle: filmInfo['alternative_title'],
+        totalRating: filmInfo['total_rating'],
+        ageRating: filmInfo['age_rating'],
+        release: {
+          ...filmInfo['release'],
+          releaseCountry: filmInfo['release']['release_country']
+        }
+      },
+      userDetails: {
+        ...film['user_details'],
+        watchList: film['user_details']['watchlist'],
+        alreadyWatched: film['user_details']['already_watched'],
+        watchingDate: film['user_details']['watching_date']
+      }
     }
+
+    this.#deleteUnusedProps(adaptedFilm);
+
+    return adaptedFilm;
   }
 
   get = () => {
--- src/services/films-api-service.js
+++ src/services/films-api-service.js
@@ -12,21 +12,52 @@ export default class FilmsApiService extends ApiService{
       .then(ApiService.parseResponse);
   }
 
+  #deleteUnusedProps = (obj) => {
+    delete obj.alternativeTitle;
+    delete obj.totalRating;
+    delete obj.ageRating;
+    delete obj.release.releaseCountry;
+    delete obj.user_details.watchList;
+    delete obj.user_details.alreadyWatched;
+    delete obj.user_details.watchingDate;
+  }
+
   updateFilms = async (film) => {
     const response = await this._load({
       url: `/movies/${film.id}`,
       method: METHODS.PUT,
-      body: JSON.stringify(film),
+      body: JSON.stringify(FilmsApiService.adaptToServer(film)),
       headers: new Headers({'Content-Type': 'application/json'})
     })
 
     return await ApiService.parseResponse(response);
   }
 
-  #adaptToServer = (film) => {
+  static adaptToServer = (film) => {
+    const filmInfo = film.filmInfo
+
     const adaptedFilm = {
       ...film,
-
+      'film_info': {
+        ...filmInfo,
+        'alternative_title': filmInfo.alternativeTitle,
+        'total_rating': filmInfo.totalRating,
+        'age_rating': filmInfo.ageRating,
+        release: {
+          ...filmInfo.release,
+          'release_country': filmInfo.release.releaseCountry
+        },
+        'user_details': {
+          ...filmInfo.userDetails,
+          'watchlist': filmInfo.userDetails.watchList,
+          'already_watched': filmInfo.userDetails.alreadyWatched,
+          'watching_date': filmInfo.userDetails.watchingDate
+        }
+      }
     }
+
+    this.#deleteUnusedProps(adaptedFilm);
+
+    return adaptedFilm;
   }
 }
